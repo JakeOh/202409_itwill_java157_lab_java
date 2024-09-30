dc337c37188aa00f925f95caab70edf431e304f3
JakeOh <jake.oh.lecture@gmail.com>
2024-09-30 14:22:01 +0900

    variable

---
+++ lab02_variable/src/com/itwill/variable04/VariableMain04.java
@@ -0,0 +1,11 @@
+package com.itwill.variable04;
+
+public class VariableMain04 {
+
+    public static void main(String[] args) {
+        // 문자(character) 타입 char:
+        // 문자 1개의 유니코드 값(2바이트 정수)를 저장하는 타입.
+
+    }
+
+}
