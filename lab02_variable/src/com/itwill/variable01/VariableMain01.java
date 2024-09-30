49e7d5f84a2bc6718ffbadeed2bd62324ce214cd
itwill <itwill@DESKTOP-GAIH3D6>
2024-09-30 10:17:48 +0900

    variable

---
+++ lab02_variable/src/com/itwill/variable01/VariableMain01.java
@@ -0,0 +1,17 @@
+package com.itwill.variable01; // 패키지 선언문
+
+// 클래스 선언
+public class VariableMain01 {
+	
+	// 메인 메서드
+	public static void main(String[] args) {
+		// 변수(variable) 선언/초기화/사용하기
+		
+		// 변수 선언과 동시에 초기화(변수 선언과 초기화를 한 문장으로 작성).
+		// 변수타입 변수이름 = 값;
+		int x = 100;
+		System.out.println("x = " + x);
+		
+	}
+
+}
