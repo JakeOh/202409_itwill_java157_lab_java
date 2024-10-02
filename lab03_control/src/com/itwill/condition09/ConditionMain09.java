f655a34797ea9e7525828bf5e4acbb2ca939b9a1
JakeOh <jake.oh.lecture@gmail.com>
2024-10-02 17:58:05 +0900

    conditional statement, ternary operator

--- lab03_control/src/com/itwill/condition09/ConditionMain09.java
+++ lab03_control/src/com/itwill/condition09/ConditionMain09.java
@@ -18,9 +18,23 @@ public class ConditionMain09 {
         System.out.println(number);
         
         // 주사위 2개를 던져서 나온 눈을 변수(들)에 저장.
-        // int dice1 = ?; int dice2 = ?;
+        int dice1 = rand.nextInt(1, 7); // 1 이상 7 미만의 난수를 저장.
+        int dice2 = rand.nextInt(1, 7);
+        
         // 두 주사위의 숫자들을 출력.
+        System.out.printf("(%d, %d)\n", dice1, dice2);
+        
         // 두 주사위 숫자의 차이를 출력.
+        int diff;
+        if (dice1 > dice2) {
+            diff = dice1 - dice2;
+        } else {
+            diff = dice2 - dice1;
+        }
+        System.out.println(diff);
+        
+        int diff2 = (dice1 > dice2) ? (dice1 - dice2) : (dice2 - dice1);
+        System.out.println(diff2);
         
     }
 
