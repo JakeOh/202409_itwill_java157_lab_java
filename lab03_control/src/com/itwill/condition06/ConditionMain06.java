928e518d33275fcf6730706ba117c77be7091e1a
JakeOh <jake.oh.lecture@gmail.com>
2024-10-02 11:17:13 +0900

    if-else

---
+++ lab03_control/src/com/itwill/condition06/ConditionMain06.java
@@ -0,0 +1,20 @@
+package com.itwill.condition06;
+
+import java.util.Scanner;
+
+public class ConditionMain06 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Scanner 타입의 변수를 생성하고 초기화.
+        
+        System.out.print("첫번째 정수 입력>>> ");
+        int x = scanner.nextInt(); // 콘솔창에서 입력한 정수를 변수 x에 저장.
+        
+        System.out.print("두번째 정수 입력>>> ");
+        int y = scanner.nextInt();
+
+        int big; // x, y 중에서 더 큰(크거나 같은) 정수를 저장하기 위한 변수.
+        
+    }
+
+}
