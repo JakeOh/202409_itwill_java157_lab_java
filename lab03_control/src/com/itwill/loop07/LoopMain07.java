1e092075825cc1ad8803831123dcbfe4c147da17
JakeOh <jake.oh.lecture@gmail.com>
2024-10-07 12:07:40 +0900

    nested loop

---
+++ lab03_control/src/com/itwill/loop07/LoopMain07.java
@@ -0,0 +1,11 @@
+package com.itwill.loop07;
+
+public class LoopMain07 {
+
+    public static void main(String[] args) {
+        // 구구단 출력: 2단은 2x2까지, 3단은 3x3까지, ..., 9단은 9x9까지 출력.
+        // (1) break 사용.
+        // (2) break 사용하지 않기.
+    }
+
+}
