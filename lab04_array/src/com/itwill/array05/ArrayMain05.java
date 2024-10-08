0655d9fc3bfef196b5e1158ba3abae7e43a0187f
JakeOh <jake.oh.lecture@gmail.com>
2024-10-08 17:36:13 +0900

    array exercise

---
+++ lab04_array/src/com/itwill/array05/ArrayMain05.java
@@ -0,0 +1,24 @@
+package com.itwill.array05;
+
+public class ArrayMain05 {
+
+    public static void main(String[] args) {
+        // 난수를 생성하는 Random 타입 변수를 선언하고, 초기화.
+        
+        // 정수(int) 10개를 저장할 수 있는 배열 codes를 선언하고, (기본값으로) 초기화.
+        
+        // for 문장을 사용해서, 배열 codes에 1 이상 4 이하의 정수 난수들을 저장(10번 반복).
+        
+        // 배열 codes의 원소들을 출력.
+        
+        // 문자열(String) 10개를 저장할 수 있는 배열 genders를 선언하고, (기본값으로) 초기화.
+        
+        // 배열 genders에는 배열 codes의 값에 따라서 문자열을 저장:
+        // -> codes의 원소가 1또는 3이면 "남성"을, codes의 원소가 2또는 4이면 "여성"을 저장.
+        // -> 만약 codes가 {1, 4, 2, 3} 이면, genders는 {"남성", "여성", "여성", "남성}.
+
+        // 배열 genders의 원소들을 출력.
+        
+    }
+
+}
