7d53790da7114911ca5f7109cb4b63ce384c204f
JakeOh <jake.oh.lecture@gmail.com>
2024-10-08 10:18:34 +0900

    loop exercises

--- lab03_control/src/com/itwill/loop14/LoopMain14.java
+++ lab03_control/src/com/itwill/loop14/LoopMain14.java
@@ -3,8 +3,42 @@ package com.itwill.loop14;
 public class LoopMain14 {
 
     public static void main(String[] args) {
-        // 과제: 교재 연습문제 5, 6, 7
-
+        // 교재 연습문제 5
+        for (int x = 1; x <= 4; x++) { // x: line 번호
+            for (int y = 1; y <= x; y++) { // y: 출력할 "*"의 개수
+                System.out.print("*");
+            }
+            System.out.println(); // 줄바꿈
+        }
+        
+        System.out.println("----------");
+        
+        for (int x = 1; x <= 4; x++) { // x: 줄 번호
+            for (int y = 1; y <= 4; y++) { // y: 출력할 *의 개수
+                System.out.print("*");
+                if (y == x) { // 출력한 * 개수가 줄 번호와 같으면
+                    break; // 출력을 멈춤.
+                }
+            }
+            System.out.println();
+        }
+        
+        System.out.println("----------");
+        
+        // 교재 연습문제 6.
+        for (int x = 1; x <= 4; x++) { // x: 줄 번호
+            for (int y = 1; y <= 4; y++) { // y: print()를 호출할 개수
+                if (y <= 4 - x) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        
+        System.out.println("----------");
+        
     }
 
 }
