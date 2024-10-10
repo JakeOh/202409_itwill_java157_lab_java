8398225f76c36b1e7a8cede73ce5c635477f2d44
JakeOh <jake.oh.lecture@gmail.com>
2024-10-10 11:18:11 +0900

    array exercise

---
+++ lab04_array/src/com/itwill/array07/ArrayMain07.java
@@ -0,0 +1,18 @@
+package com.itwill.array07;
+
+public class ArrayMain07 {
+
+    public static void main(String[] args) {
+        // 정수 5개를 저장할 수 있는 배열 numbers을 선언, (기본값으로) 초기화.
+        
+        // Random 타입의 변수를 선언, 초기화.
+        
+        // 배열 numbers에 0이상 10 미만의 난수 5개를 저장.
+        
+        // 배열 numbers의 원소들 중 최댓값을 찾고 출력.
+        
+        // 배열 numbers의 원소들 중 최솟값을 찾고 출력.
+
+    }
+
+}
