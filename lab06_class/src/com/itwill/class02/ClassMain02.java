bd00f01ce59f12c20e123bbae69a7427d241f1fc
JakeOh <jake.oh.lecture@gmail.com>
2024-10-14 17:27:27 +0900

    object, class, field, constructor, method

--- lab06_class/src/com/itwill/class02/ClassMain02.java
+++ lab06_class/src/com/itwill/class02/ClassMain02.java
@@ -7,14 +7,36 @@ public class ClassMain02 {
         // Score 타입의 객체 선언, Score 객체 생성하고 변수에 할당.
         Score score1 = new Score();
         System.out.println(score1);
+        System.out.println("korean = " + score1.korean);
         
         // score1 인스턴스의 필드 값들을 변경.
         score1.korean = 100;
         score1.english = 90;
-        score1.math = 50;
+        score1.math = 55;
+        System.out.println("korean = " + score1.korean);
         
         // score1 인스턴스의 메서드 호출:
-        System.out.println("총점: " + score1.getTotal());
+        System.out.println("score1 총점: " + score1.getTotal());
+        System.out.println("score1 평균: " + score1.getMean());
+        
+        // Score 타입 객체 선언, 초기화.
+        Score score2 = new Score();
+        
+        // score2 객체(인스턴스)의 필드들을 변경.
+        score2.korean = 50;
+        score2.english = 55;
+        score2.math = 99;
+        
+        // score2 인스턴스의 메서드들을 호출.
+        System.out.println("score2 총점: " + score2.getTotal());
+        System.out.println("score2 평균: " + score2.getMean());
+        
+        // Score 타입 변수 선언, 아규먼트를 갖는 생성자를 호출해서 초기화.
+        Score score3 = new Score(99, 88, 70);
+        
+        // score3 인스턴스에서 메서드 호출.
+        System.out.println("score3 총점: " + score3.getTotal());
+        System.out.println("score3 평균: " + score3.getMean());
         
     }
 
--- lab06_class/src/com/itwill/class02/Score.java
+++ lab06_class/src/com/itwill/class02/Score.java
@@ -8,10 +8,26 @@ public class Score {
     int english;
     int math;
     
+    // 생성자 선언:
+    public Score() {
+        // 실행문이 없으면 필드의 모든 값들은 그 타입의 기본값으로 초기화됨.
+    }
+    
+    public Score(int korean, int english, int math) {
+        // this: Score 타입으로 생성된 객체.
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+    
     // 메서드 선언:
     // 메서드는 필드에 할당(저장)된 값들을 사용할 수 있음.
     public int getTotal() {
         return korean + english + math;
+    }
+    
+    public double getMean() {
+        return (double) getTotal() / 3;
     }
     
 }
