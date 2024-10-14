abd3b44ec4ce290643996644c112422d0ef3509a
JakeOh <jake.oh.lecture@gmail.com>
2024-10-14 11:19:25 +0900

    method overloading

---
+++ lab05_method/src/com/itwill/method06/MethodMain06.java
@@ -0,0 +1,17 @@
+package com.itwill.method06;
+
+public class MethodMain06 {
+
+    public static void main(String[] args) {
+        // 메서드 오버로딩(method overloading):
+        // 메서드의 파라미터가 다르면 같은 이름으로 메서드를 여러개 선언(정의)할 수 있음.
+        // (주의) 메서드의 파라미터는 동일하고 리턴 타입이 다른 메서드는 오버로딩할 수 없음!
+
+        System.out.println("안녕하세요.");
+        System.out.println(100);
+        System.out.println();
+        System.out.println(true);
+        
+    }
+
+}
