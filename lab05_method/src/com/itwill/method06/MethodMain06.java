72f53c696e2bd1ac3f3f7d044a39dd50552b2ad5
JakeOh <jake.oh.lecture@gmail.com>
2024-10-14 12:16:53 +0900

    method overloading

--- lab05_method/src/com/itwill/method06/MethodMain06.java
+++ lab05_method/src/com/itwill/method06/MethodMain06.java
@@ -12,6 +12,34 @@ public class MethodMain06 {
         System.out.println();
         System.out.println(true);
         
+        System.out.println("----------");
+        newLine();
+        newLine(2);
+        System.out.println("----------");
+    }
+    
+    /**
+     * 콘솔에 빈 줄 한 개를 출력.
+     */
+    public static void newLine() {
+        System.out.println();
+    }
+    
+    // 파라미터가 동일하고, 리턴 타입만 다른 메서드는 오버로딩할 수 없음!
+//    public static int newLine() {
+//        System.out.println();
+//        return 1;
+//    }
+    
+    /**
+     * 콘솔에 빈 줄 n개를 출력.
+     * 
+     * @param n 출력한 빈 줄의 개수. 양의 정수.
+     */
+    public static void newLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+        }
     }
 
 }
