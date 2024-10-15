e253db32dfbc97c2f7239a3ad78a621499b22a3f
JakeOh <jake.oh.lecture@gmail.com>
2024-10-15 14:24:19 +0900

    class members: field, constructor, method

--- lab06_class/src/com/itwill/class05/ClassMain05.java
+++ lab06_class/src/com/itwill/class05/ClassMain05.java
@@ -3,7 +3,14 @@ package com.itwill.class05;
 public class ClassMain05 {
 
     public static void main(String[] args) {
-        // TODO: Subject, Student 객체를 생성하고, 메서드 호출.
+        // Subject 타입 변수 선언, 초기화.
+        Subject subject1 = new Subject();
+        System.out.println(subject1);
+        subject1.info();
+        
+        Subject subject2 = new Subject(100, 90, 85, 75);
+        System.out.println(subject2);
+        subject2.info();
 
     }
 
--- lab06_class/src/com/itwill/class05/Subject.java
+++ lab06_class/src/com/itwill/class05/Subject.java
@@ -9,11 +9,36 @@ public class Subject {
 
     // constructor
     // (1) 기본 생성자
+    public Subject() {}
+    
     // (2) 4과목의 점수를 초기화하는 (아규먼트 4개를 갖는) 생성자
+    public Subject(int java, int sql, int html, int javascript) {
+        this.java = java;
+        this.sql = sql;
+        this.html = html;
+        this.javascript = javascript;
+    }
     
     // method
     // (1) getTotal: 4과목의 총점을 리턴.
+    public int getTotal() {
+        return this.java + this.sql + this.html + this.javascript;
+//        return java + sql + html + javascript;
+    }
+    
     // (2) getMean: 4과목의 평균(double)을 리턴.
+    public double getMean() {
+        return (double) getTotal() / 4;
+    }
+    
     // (3) info: 4과목의 점수, 총점, 평균을 출력.
+    public void info() {
+        System.out.println("Java: " + this.java);
+        System.out.println("SQL: " + this.sql);
+        System.out.println("HTML: " + this.html);
+        System.out.println("JavaScript: " + this.javascript);
+        System.out.println("총점: " + this.getTotal());
+        System.out.println("평균: " + this.getMean());
+    }
     
 }
