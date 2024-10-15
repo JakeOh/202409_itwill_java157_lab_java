ff9433aa576bbd000bc1c9b6dcab382eaabb4bab
JakeOh <jake.oh.lecture@gmail.com>
2024-10-15 15:32:31 +0900

    class members: field, constructors, method

--- lab06_class/src/com/itwill/class05/ClassMain05.java
+++ lab06_class/src/com/itwill/class05/ClassMain05.java
@@ -11,6 +11,18 @@ public class ClassMain05 {
         Subject subject2 = new Subject(100, 90, 85, 75);
         System.out.println(subject2);
         subject2.info();
+        
+        // Student 타입 변수 선언, 초기화
+        Student student1 = new Student();
+        System.out.println(student1);
+        student1.info();
+        
+        Student student2 = new Student(1, "오쌤", subject1);
+        System.out.println(student2);
+        student2.info();
+        
+        Student student3 = new Student(2, "김민석", subject2);
+        student3.info();
 
     }
 
--- lab06_class/src/com/itwill/class05/Student.java
+++ lab06_class/src/com/itwill/class05/Student.java
@@ -8,9 +8,27 @@ public class Student {
     
     // constructor
     // (1) 기본 생성자
+    public Student() {}
+    
     // (2) 번호/이름/과목점수를 초기화하는(아규먼트 3개를 갖는) 생성자
+    public Student(int id, String name, Subject subject) {
+        this.id = id;
+        this.name = name;
+        this.subject = subject;
+    }
     
     // method
     // (1) info: 학생 번호, 이름, 과목 점수들, 총점, 평균을 출력.
+    public void info() {
+        System.out.println("--- 학생 정보 ---");
+        System.out.println("번호: " + this.id);
+        System.out.println("이름: " + this.name);
+        if (this.subject != null) {
+            this.subject.info();
+        } else {
+            System.out.println("과목: null");
+        }
+        System.out.println("------------------");
+    }
     
 }
