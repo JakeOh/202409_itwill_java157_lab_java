6108bea3fc2b66b0bc776be84789dd75ec09ce4b
JakeOh <jake.oh.lecture@gmail.com>
2024-10-21 14:16:01 +0900

    BasicTv methods

--- lab07_inheritance/src/com/itwill/inheritance01/BasicTv.java
+++ lab07_inheritance/src/com/itwill/inheritance01/BasicTv.java
@@ -1,6 +1,12 @@
 package com.itwill.inheritance01;
 
 public class BasicTv {
+    // 상수 선언
+    public static final int MAX_CHANNEL = 3;
+    public static final int MIN_CHANNEL = 0;
+    public static final int MAX_VOLUME = 3;
+    public static final int MIN_VOLUME = 0;
+    
     // field
     private boolean powerOn; // 전원 상태(true: ON, false: OFF)
     private int channel; // 현재 채널 번호
@@ -8,14 +14,110 @@ public class BasicTv {
     
     // 생성자
     // (1) 기본 생성자
+    public BasicTv() {}
+    
     // (2) powerOn, channel, volume을 아규먼트로 전달받는 생성자.
+    public BasicTv(boolean powerOn, int channel, int volume) {
+        this.powerOn = powerOn;
+        this.channel = channel;
+        this.volume = volume;
+    }
     
     // method
     // (1) getters & setters
-    // (2) powerOnOff: TV가 꺼져 있으면 키고, 켜져 있으면 끔. 현재 전원 상태를 리턴.
-    // (3) channelUp: 채널 값을 1 증가. 증가된 채널 값을 리턴.
-    // (4) channelDown: 채널 값을 1 감소. 감소된 채널 값을 리턴.
-    // (5) volumeUp: 음량 1 증가. 증가된 음량 값을 리턴.
-    // (6) volumeDown: 음량 1 감소. 감소된 음량 값을 리턴.
-    // (7) toString: TV 객체의 정보를 문자열로 리턴.
+    public boolean isPowerOn() {
+        return this.powerOn;
+    }
+    
+    public void setPowerOn(boolean powerOn) {
+        this.powerOn = powerOn;
+    }
+    
+    public int getChannel() {
+        return this.channel;
+    }
+    
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
+    
+    public int getVolume() {
+        return this.volume;
+    }
+    
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+    /**
+     * powerOnOff. TV가 켜져 있으면 끄고, TV가 꺼져 있는 켬.
+     * 필드 powerOn의 값이 true이면 false로 변경, powerOn의 값이 false이면 true로 변경.
+     * 
+     * @return powerOn의 값.
+     */
+    public boolean powerOnOff() {
+        if (powerOn) { // TV가 켜져 있으면
+            powerOn = false; // TV를 끔.
+        } else { // TV가 꺼져 있으면
+            powerOn = true; // TV를 켬.
+        }
+        
+        return powerOn;
+    }
+    
+    /**
+     * channelUp. TV가 켜져 있을 때 현재 채널의 값을 1 증가.
+     * 만약 현재 채널 값이 채널의 최댓값(MAX_CHANNEL)인 경우, 채널의 최솟값(MIN_CHANNEL)으로 변경.
+     * 
+     * @return 바뀐 현재 채널 값.
+     */
+    public int channelUp() {
+        channel++;
+        
+        return channel;
+    }
+    
+    /**
+     * channelDown. TV가 켜져 있을 때 현재 채널의 값을 1 감소.
+     * 만약 현재 채널 값이 채널의 최솟값(MIN_CHANNEL)인 경우, 채널의 최댓값(MAX_CHANNEL)으로 변경.
+     * 
+     * @return 바뀐 현재 채널 값.
+     */
+    public int channelDown() {
+        channel--;
+        
+        return channel;
+    }
+    
+    /**
+     * volumeUp. TV가 켜져 있을 때 현재 음량의 값을 1 증가.
+     * 만약 현재 음량 값이 음량의 최댓값(MAX_VOLUME)인 경우, 음량을 변경하지 않음.
+     * 
+     * @return 바뀐 현재 음량 값.
+     */
+    public int volumeUp() {
+        volume++;
+        
+        return volume;
+    }
+    
+    /**
+     * volumeDown. TV가 켜져 있을 때 현재 음량의 값을 1 감소.
+     * 만약 현재 음량 값이 음량의 최솟값(MIN_VOLUME)인 경우, 음량을 변경하지 않음.
+     * 
+     * @return 바뀐 현재 음량 값.
+     */
+    public int volumeDown() {
+        volume--;
+        
+        return volume;
+    }
+    
+    public String toString() {
+        return "BasicTv(powerOn=" + this.powerOn
+                + ", channel=" + this.channel
+                + ", volume=" + this.volume
+                + ")";
+    }
+    
 }
--- lab07_inheritance/src/com/itwill/inheritance01/InheritanceMain01.java
+++ lab07_inheritance/src/com/itwill/inheritance01/InheritanceMain01.java
@@ -3,8 +3,19 @@ package com.itwill.inheritance01;
 public class InheritanceMain01 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        // BasicTv 타입 변수 선언, 초기화.
+        BasicTv tv1 = new BasicTv();
+        System.out.println(tv1.toString());
+        
+        tv1.powerOnOff();
+        System.out.println(tv1.toString());
+        
+        tv1.channelUp();
+        tv1.channelUp();
+        System.out.println(tv1.toString());
+        
+        tv1.powerOnOff();
+        System.out.println(tv1.toString());
     }
 
 }
