bfc5f46b4608610a0bdee9e84fc9a59640edd1ec
JakeOh <jake.oh.lecture@gmail.com>
2024-10-21 17:32:04 +0900

    inheritance and constructors

--- lab07_inheritance/src/com/itwill/inheritance02/InheritanceMain02.java
+++ lab07_inheritance/src/com/itwill/inheritance02/InheritanceMain02.java
@@ -3,8 +3,16 @@ package com.itwill.inheritance02;
 public class InheritanceMain02 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // BusinessPerson 타입 객체 선언, 초기화.
+        BusinessPerson p1 = new BusinessPerson();
+        
+        System.out.println("-----");
+        
+        BusinessPerson p2 = new BusinessPerson("아이티윌");
 
+        System.out.println("-----");
+        
+        BusinessPerson p3 = new BusinessPerson("오쌤", "아이티윌");
     }
 
 }
