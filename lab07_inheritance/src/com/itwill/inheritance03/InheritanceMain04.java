31dd774c26dc1bc7859a9ace587631eb2665feb2
JakeOh <jake.oh.lecture@gmail.com>
2024-10-22 14:23:48 +0900

    final method/class

---
+++ lab07_inheritance/src/com/itwill/inheritance03/InheritanceMain04.java
@@ -0,0 +1,20 @@
+package com.itwill.inheritance03;
+
+/*
+ * final: 변경할 수 없는.
+ * 1. final 지역 변수: 값을 초기화한 후에는 그 값을 변경할 수 없는(재할당할 수 없는) 지역 변수.
+ * 2. final 필드: 반드시 한 번 명시적으로 초기화해야 하고, 초기화한 후에는 값을 변경할 수 없는 필드.
+ *    (1) final 필드를 선언과 동시에 초기화.
+ *    (2) 생성자에서 final 필드를 명시적으로 초기화.
+ * 3. final 메서드: 변경할 수 없는 메서드. 재정의(override)할 수 없는 메서드.
+ * 4. final 클래스: 변경할 수 없는 클래스. 상속(확장)할 수 없는 클래스.
+ */
+
+public class InheritanceMain04 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
