87babdff13128f43bf73c9ec6130615219b5b497
JakeOh <jake.oh.lecture@gmail.com>
2024-10-23 15:14:57 +0900

    interface

---
+++ lab07_inheritance/src/com/itwill/interface01/InterfaceMain01.java
@@ -0,0 +1,14 @@
+package com.itwill.interface01;
+
+/*
+ * 
+ */
+
+public class InterfaceMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
