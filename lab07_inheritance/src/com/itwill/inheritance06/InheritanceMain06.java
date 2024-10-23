f4f2d4b1e5f26ef04b85f8c5487391fae64298af
JakeOh <jake.oh.lecture@gmail.com>
2024-10-23 10:33:14 +0900

    toString, hashCode, equals

--- lab07_inheritance/src/com/itwill/inheritance06/InheritanceMain06.java
+++ lab07_inheritance/src/com/itwill/inheritance06/InheritanceMain06.java
@@ -3,8 +3,31 @@ package com.itwill.inheritance06;
 public class InheritanceMain06 {
 
     public static void main(String[] args) {
-        // TODO Point 타입의 객체(들)을 생성, toString, equals, hashCode 메서드 테스트.
+        // Point 타입의 객체(들)을 생성, toString, equals, hashCode 메서드 테스트.
+        Point pt1 = new Point();
+        
+        System.out.println(pt1);
+        //-> toString() 오버라이드 전: com.itwill.inheritance06.Point@54bedef2
+        //-> toString() 오버라이드 후: Point(x=0.0, y=0.0)
+        
+        System.out.println(pt1.hashCode());
+        //-> hashCode() 오버라이드 전: 1421795058(10진수) = 0x54bedef2(16진수)
+        //-> hashCode() 오버라이드 후: 961
+        
+        Point pt2 = new Point(0, 0);
+        System.out.println(pt2);
+        System.out.println(pt2.hashCode());
+        //-> hashCode()를 오버라이드한 후에는 pt1의 hashCode와 pt2의 hashCode는 같아야.
 
+        System.out.println(pt1.equals(pt2));
+        //-> equals() 오버라이드 전: false
+        //-> equals() 오버라이드 후: true
+        
+        Point pt3 = new Point(1, 2);
+        System.out.println(pt3);
+        System.out.println(pt3.hashCode());
+        System.out.println(pt1.equals(pt3));
+        
     }
 
 }
--- lab07_inheritance/src/com/itwill/inheritance06/Point.java
+++ lab07_inheritance/src/com/itwill/inheritance06/Point.java
@@ -1,5 +1,7 @@
 package com.itwill.inheritance06;
 
+import java.util.Objects;
+
 // 2차원 평면에서의 점의 좌표를 나타내는 객체.
 public class Point {
     // field
@@ -7,9 +9,49 @@ public class Point {
     private double y;
     
     // constructor: (1) 기본 생성자, (2) x, y를 아규먼트로 갖는 생성자
+    public Point() {}
     
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
     // (Object 클래스에서 상속받은) toString() 메서드를 override:
     // 리턴하는 문자열에는 필드 x, y의 값이 표현되야 함.
+    @Override
+    public String toString() {
+        return "Point(x=" + x + ", y=" + y + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if (obj instanceof Point) {
+            Point other = (Point) obj;
+            result = (this.x == other.x) && (this.y == other.y);
+        }
+        
+        return result;
+        
+        /*
+        if (this == obj) // 두 객체의 주소값이 같으면
+            return true;
+        if (obj == null) // 아규먼트가 null이면
+            return false;
+        if (getClass() != obj.getClass()) // 두 객체의 클래스 타입이 다르면
+            return false;
+        Point other = (Point) obj; // 아규먼트를 Point 타입으로 강제 타입 변환(casting)
+        
+        // 필드들의 값을 각각 비교
+        return Double.doubleToLongBits(x) == Double.doubleToLongBits(other.x)
+                && Double.doubleToLongBits(y) == Double.doubleToLongBits(other.y);
+        */
+    }
     
     // equals() 메서드를 override:
     // 필드 x, y의 값이 서로 같으면 true, 그렇지 않으면 false.
