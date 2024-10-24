0fb4d395f0f6f87e8559f227786d8dd5174a6318
JakeOh <jake.oh.lecture@gmail.com>
2024-10-24 17:26:22 +0900

    Blog app with MVC

---
+++ lab08_mvc/src/com/itwill/blog/view/BlogMain.java
@@ -0,0 +1,11 @@
+package com.itwill.blog.view;
+
+// MVC 아키텍쳐에서 View(UI, 입/출력)를 담당하는 클래스.
+public class BlogMain {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
