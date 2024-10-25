af499d1e9f944b9caa83b69bd92ce760b429dd3d
JakeOh <jake.oh.lecture@gmail.com>
2024-10-25 11:17:44 +0900

    create blog

--- lab08_mvc/src/com/itwill/blog/controller/BlogDaoImpl.java
+++ lab08_mvc/src/com/itwill/blog/controller/BlogDaoImpl.java
@@ -19,11 +19,19 @@ public class BlogDaoImpl implements BlogDao {
     }
     //<----- singleton 구현 끝
 
+    private static final int MAX_LENGTH = 3; // 블로그 배열의 길이(원소 개수).
+    
+    private Blog[] blogs = new Blog[MAX_LENGTH]; // 블로그들을 저장할 배열.
+    
+    private int count = 0; // 배열 blogs에 저장된 원소 개수. 새 블로그가 저장될 때마다 ++.
+    
     @Override
     public int create(Blog blog) {
-        System.out.println("새 블로그 작성");
-        // TODO Auto-generated method stub
-        return 0;
+        // 배열 blogs에 원소를 저장하고 count를 증가.
+        blogs[count] = blog;
+        count++;
+        
+        return 1;
     }
 
     @Override
--- lab08_mvc/src/com/itwill/blog/view/BlogMain.java
+++ lab08_mvc/src/com/itwill/blog/view/BlogMain.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 //import com.itwill.blog.controller.BlogController;
 import com.itwill.blog.controller.BlogDao;
 import com.itwill.blog.controller.BlogDaoImpl;
+import com.itwill.blog.model.Blog;
 
 // MVC 아키텍쳐에서 View(UI, 입/출력)를 담당하는 클래스.
 public class BlogMain {
@@ -47,8 +48,28 @@ public class BlogMain {
     }
 
     private void createNewBlog() {
-        // TODO
-        dao.create(null);
+        System.out.println("\n--- 새 블로그 작성 ---");
+        
+        // 1. 새 블로그 작성에 필요한 제목/내용/작성자를 입력:
+        System.out.print("제목> ");
+        String title = scanner.nextLine();
+        
+        System.out.print("내용> ");
+        String content = scanner.nextLine();
+        
+        System.out.print("작성자> ");
+        String author = scanner.nextLine();
+        
+        // 2. Blog 타입 객체 생성:
+        Blog blog = new Blog(title, content, author); 
+        
+        // 3. MVC 아키텍쳐에서 Controller의 메서드를 호출해서 블로그 작성(저장) 기능을 수행.
+        int result = dao.create(blog);
+        if (result == 1) {
+            System.out.println(">> 새 블로그 작성 성공");
+        } else {
+            System.out.println(">> 새 블로그 작성 실패");
+        }
     }
     
     private MainMenu selectMainMenu() {
