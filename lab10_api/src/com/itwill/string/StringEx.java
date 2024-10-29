5228c8a39ba317f831b6949a70d802c952722ab3
JakeOh <jake.oh.lecture@gmail.com>
2024-10-29 10:12:51 +0900

    String methods

--- lab10_api/src/com/itwill/string/StringEx.java
+++ lab10_api/src/com/itwill/string/StringEx.java
@@ -20,22 +20,45 @@ public class StringEx {
         System.out.println("Ex. 3 ---");
         String[] languages = {"Java", "SQL", "JavaScript", "Python", "Kotlin", "HTML"};
         // 배열 languages에서 5글자 이상인 문자열들만 출력.
+        for (String s : languages) {
+            if (s.length() >= 5) {
+                System.out.print(s + " ");
+            }
+        }
+        System.out.println();
         
         System.out.println("Ex. 4 ---");
-        String[] names = {"오쌤", "홍길동", "Gildong", "홍길동"};
-        // 배열 names에서 문자열 "홍길동"이 처음 등장하는 인덱스를 출력: 1
+        String[] names = {"오쌤", "홍길동", "Gildong"};
+        // 배열 names에서 문자열 "홍길동"이 처음 등장하는 인덱스를 출력.
+        // 배열에 "홍길동" 문자열이 없으면 -1을 출력.
+        int index = -1;
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].equals("홍길동")) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println("index = " + index);
         
         System.out.println("Ex. 5 ---");
         String[] tests = {"테스트", "test", "Test", "문자열 TEST", "tEST"};
         // 배열 tests에서 대소문자 구분없이 "est"를 포함하는 문자열들을 출력.
+        for (String s : tests) {
+            if (s.toLowerCase().contains("est")) {
+                System.out.print(s + "\t");
+            }
+        }
+        System.out.println();
         
-        System.out.println("Ex. 6 ----");
+        System.out.println("Ex. 6 ---");
         String today = "2024/10/28";
         // 문자열 today(YYYY/MM/DD)에서 년/월/일 정보를 int 타입 변수(들)에 저장.
         // 년/월/일을 출력.
-        int year;
-        int month;
-        int day;
+        String[] strArray = today.split("/");
+        int year = Integer.parseInt(strArray[0]);
+        int month = Integer.parseInt(strArray[1]);
+        int day = Integer.parseInt(strArray[2]);
+        System.out.printf("year=%d, month=%d, day=%d\n", year, month, day);
         
     }
 
