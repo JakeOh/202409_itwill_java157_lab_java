7710535d6404002c80784d61fc02bd039a327706
JakeOh <jake.oh.lecture@gmail.com>
2024-10-29 15:33:09 +0900

    List exercise

---
+++ lab11_collection/src/com/itwill/list03/ListMain03.java
@@ -0,0 +1,17 @@
+package com.itwill.list03;
+
+public class ListMain03 {
+
+    public static void main(String[] args) {
+        // 1. 정수를 저장할 수 있는 List(ArrayList 또는 LinkedList)를 선언(numbers), 객체 생성.
+        // 2. numbers에 [0, 100) 범위(0 이상 100 미만)의 난수 20개를 저장.
+        // 3. numbers를 출력.
+        // 4. 정수를 저장할 수 있는 ArrayList를 선언(odds), 객체 생성.
+        // 5. numbers에 저장된 숫자들 중에서 홀수들을 찾아서 odds에 저장.
+        // 6. odds를 출력.
+        // 7. 정수를 저장할 수 있는 ArrayList를 선언(evenSquares), 객체 생성.
+        // 8. numbers에 저장된 숫자들 중에서 짝수를 찾아서 짝수의 제곱을 evenSquares에 저장.
+        // 9. evenSquares를 출력.
+    }
+
+}
