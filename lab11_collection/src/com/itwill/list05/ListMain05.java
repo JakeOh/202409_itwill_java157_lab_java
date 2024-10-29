4727e3f6bb65852839b95fb4dbeee64efd6500aa
JakeOh <jake.oh.lecture@gmail.com>
2024-10-29 17:17:45 +0900

    list homework

---
+++ lab11_collection/src/com/itwill/list05/ListMain05.java
@@ -0,0 +1,24 @@
+package com.itwill.list05;
+
+import java.util.Scanner;
+
+public class ListMain05 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Member 타입을 저장하는 리스트 선언, 객체 생성.
+
+        // 5번 반복. id와 password를 입력. Member 타입 객체 생성. 리스트에 Member 객체를 저장.
+        
+        // Member 리스트의 원소들을 출력.
+        
+        // id 검색어를 입력 받고, id에 검색어가 포함된 멤버들의 리스트를 만들고 출력.
+        // 검색어는 대/소문자 구분없이 검색이 가능해야 함.
+        
+        // 삭제할 id를 입력 받고, 처음 id가 일치하는 멤버 객체를 리스트에서 삭제.
+        // 삭제 후 리스트를 출력.
+        
+    }
+
+}
---
+++ lab11_collection/src/com/itwill/list05/Member.java
@@ -0,0 +1,16 @@
+package com.itwill.list05;
+
+public class Member {
+    // field
+    private String id;
+    private String password;
+    
+    // 생성자: (1) 기본 생성자, (2) id, password를 아규먼트로 갖는 생성자
+    
+    // getters(id, password) & setter(password)
+    
+    // toString
+    
+    // hashCode & equals - id 필드만 사용.
+
+}
