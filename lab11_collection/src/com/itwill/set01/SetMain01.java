c5bd5d6e2300ee7120045027b77a1804bb4a29c1
JakeOh <jake.oh.lecture@gmail.com>
2024-10-30 11:28:04 +0900

    Set<E>

---
+++ lab11_collection/src/com/itwill/set01/SetMain01.java
@@ -0,0 +1,27 @@
+package com.itwill.set01;
+
+/*
+ * Collection<E>
+ * |__ List<E>
+ *     |__ ArrayList<E>, LinkedList<E>, ...
+ * 
+ * Collection<E>
+ * |__ Set<E>
+ *     |__ HashSet<E>, TreeSet<E>, ...
+ * 
+ * Set<E> 특징:
+ * (1) 같은 값(equals() true인 값)을 저장하지 못함. (예) {1, 2, 2, 3, 3, 3} = {1, 2, 3}
+ * (2) 저장하는 순서가 중요하지 않음. (예) {1, 2} = {2, 1}
+ * 
+ * HashSet<E>: Hash 알고리즘(검색을 빠르게 하기 위한 알고리즘)을 사용한 집합(Set).
+ * TreeSet<E>: Tree 알고리즘(정렬을 빠르게 하기 위한 알고리즘)을 사용한 집합(Set).
+ */
+
+public class SetMain01 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
