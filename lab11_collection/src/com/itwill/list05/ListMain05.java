68c1118360c56f67f9a86d060dfb8fcbc41d3f2c
JakeOh <jake.oh.lecture@gmail.com>
2024-10-30 10:26:23 +0900

    override Object methods, List methods

--- lab11_collection/src/com/itwill/list05/ListMain05.java
+++ lab11_collection/src/com/itwill/list05/ListMain05.java
@@ -1,5 +1,6 @@
 package com.itwill.list05;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ListMain05 {
@@ -8,13 +9,37 @@ public class ListMain05 {
         Scanner scanner = new Scanner(System.in);
         
         // Member 타입을 저장하는 리스트 선언, 객체 생성.
+        ArrayList<Member> members = new ArrayList<>(); // new ArrayList<Member>();
 
         // 5번 반복. id와 password를 입력. Member 타입 객체 생성. 리스트에 Member 객체를 저장.
+        for (int i = 0; i < 5; i++) {
+            System.out.print("ID> ");
+            String id = scanner.nextLine();
+            System.out.print("Password> ");
+            String password = scanner.nextLine();
+            Member m = new Member(id, password);
+            members.add(m);
+        }
         
         // Member 리스트의 원소들을 출력.
+        System.out.println(members);
+        for (Member m : members) {
+            System.out.println(m);
+        }
         
         // id 검색어를 입력 받고, id에 검색어가 포함된 멤버들의 리스트를 만들고 출력.
         // 검색어는 대/소문자 구분없이 검색이 가능해야 함.
+        ArrayList<Member> searchResult = new ArrayList<>();
+        
+        System.out.print("ID 검색어> ");
+        String keyword = scanner.nextLine();
+        
+        for (Member m : members) {
+            if (m.getId().toLowerCase().contains(keyword.toLowerCase())) {
+                searchResult.add(m);
+            }
+        }
+        System.out.println(searchResult);
         
         // 삭제할 id를 입력 받고, 처음 id가 일치하는 멤버 객체를 리스트에서 삭제.
         // 삭제 후 리스트를 출력.
--- lab11_collection/src/com/itwill/list05/Member.java
+++ lab11_collection/src/com/itwill/list05/Member.java
@@ -1,16 +1,57 @@
 package com.itwill.list05;
 
+import java.util.Objects;
+
 public class Member {
     // field
     private String id;
     private String password;
     
     // 생성자: (1) 기본 생성자, (2) id, password를 아규먼트로 갖는 생성자
+    public Member() {}
+    
+    public Member(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
     
     // getters(id, password) & setter(password)
+    public String getId() {
+        return id;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
     // toString
+    @Override
+    public String toString() {
+        return "Member(id=" + id
+                + ", password=" + password
+                + ")";
+    }
     
     // hashCode & equals - id 필드만 사용.
+    @Override
+    public int hashCode() {
+//        return id.hashCode(); // String 클래스에서 override한 hashCode를 이용.
+        return Objects.hash(id); // java.util.Objects 클래스의 메서드를 이용.
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        boolean result = false;
+        if (o instanceof Member) {
+            Member other = (Member) o;
+            result = this.id.equals(other.id);
+        }
+        
+        return result;
+    }
 
 }
