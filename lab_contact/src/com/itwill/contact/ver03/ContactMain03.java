cb2fce1409d7940a1c1df772b68e2af1b1425bf2
JakeOh <jake.oh.lecture@gmail.com>
2024-10-30 17:44:39 +0900

    contact app v0.2

---
+++ lab_contact/src/com/itwill/contact/ver03/ContactMain03.java
@@ -0,0 +1,15 @@
+package com.itwill.contact.ver03;
+
+import java.util.Scanner;
+
+public class ContactMain03 {
+    
+    private Scanner scanner = new Scanner(System.in);
+    private ContactDao dao = ContactDao.INSTANCE;
+
+    public static void main(String[] args) {
+        // TODO 과제
+
+    }
+
+}
