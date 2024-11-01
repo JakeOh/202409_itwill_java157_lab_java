c06a4e10506d88abb049827a6e3a732add69e76d
JakeOh <jake.oh.lecture@gmail.com>
2024-11-01 15:24:13 +0900

    Reader, Writer

---
+++ lab12_file/src/com/itwill/file07/FileMain07.java
@@ -0,0 +1,30 @@
+package com.itwill.file07;
+
+/*
+ * IO Stream: 바이트 스트림(byte stream). 읽기/쓰기의 기본 단위는 바이트.
+ * - 모든 종류의 파일(txt, jpg, png, mp3, mp4, pdf, ...)에서 사용 가능.
+ * InputStream
+ * |__ FileInputStream, BufferedInputStream, ObjectInputStream, ...
+ * OutputStream
+ * |__ FileOutputStream, BufferedOutputStream, ObjectOutputStream, ...
+ * 
+ * Reader/Writer: 문자 스트림(character stream). 읽기/쓰기의 기본 단위는 문자.
+ * - 텍스트 파일 형식(txt, csv, ...)에서만 사용 가능.
+ * Reader
+ * |__ InputStreamReader, BufferedReader
+ *     |__ FileReader
+ * Writer
+ * |__ OutputStreamWriter, BufferedWriter
+ *     |__ FileWriter
+ * 
+ * 
+ */
+
+public class FileMain07 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
