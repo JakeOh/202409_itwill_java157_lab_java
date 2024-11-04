7f900aef87c76d0145d076745908c1952d8eca43
JakeOh <jake.oh.lecture@gmail.com>
2024-11-04 15:15:52 +0900

    contact app v0.4 - view

--- lab_contact/src/com/itwill/contact/ver04/view/ContactMain04.java
+++ lab_contact/src/com/itwill/contact/ver04/view/ContactMain04.java
@@ -1,6 +1,18 @@
 package com.itwill.contact.ver04.view;
 
+import java.util.Scanner;
+
+import com.itwill.contact.ver04.controller.ContactDao;
+
 public class ContactMain04 {
+    
+    private Scanner scanner;
+    private ContactDao dao;
+    
+    public ContactMain04() {
+        scanner = new Scanner(System.in);
+        dao = ContactDao.INSTANCE;
+    }
 
     public static void main(String[] args) {
         
