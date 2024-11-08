f23d3f08558731dcabc4b25d63abf40e9c9b9974
JakeOh <jake.oh.lecture@gmail.com>
2024-11-08 11:19:41 +0900

    swing, event handler

---
+++ lab15_swing/src/com/itwill/swing04/AppMain04.java
@@ -0,0 +1,114 @@
+package com.itwill.swing04;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
+public class AppMain04 {
+
+    private JFrame frame;
+    private JLabel lblName;
+    private JTextField textName;
+    private JLabel lblPhone;
+    private JTextField textPhone;
+    private JLabel lblEmail;
+    private JTextField textEmail;
+    private JButton btnInput;
+    private JTextArea textArea;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AppMain04 window = new AppMain04();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public AppMain04() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        lblName = new JLabel("이름");
+        lblName.setHorizontalAlignment(SwingConstants.CENTER);
+        lblName.setForeground(Color.WHITE);
+        lblName.setOpaque(true);
+        lblName.setBackground(SystemColor.activeCaption);
+        lblName.setFont(new Font("D2Coding", Font.BOLD, 24));
+        lblName.setBounds(12, 10, 120, 60);
+        frame.getContentPane().add(lblName);
+        
+        textName = new JTextField();
+        textName.setFont(new Font("D2Coding", Font.PLAIN, 24));
+        textName.setBounds(144, 10, 328, 60);
+        frame.getContentPane().add(textName);
+        textName.setColumns(10);
+        
+        lblPhone = new JLabel("전화번호");
+        lblPhone.setHorizontalAlignment(SwingConstants.CENTER);
+        lblPhone.setOpaque(true);
+        lblPhone.setBackground(SystemColor.activeCaption);
+        lblPhone.setForeground(Color.WHITE);
+        lblPhone.setFont(new Font("D2Coding", Font.BOLD, 24));
+        lblPhone.setBounds(12, 80, 120, 60);
+        frame.getContentPane().add(lblPhone);
+        
+        textPhone = new JTextField();
+        textPhone.setFont(new Font("D2Coding", Font.PLAIN, 24));
+        textPhone.setColumns(10);
+        textPhone.setBounds(144, 80, 328, 60);
+        frame.getContentPane().add(textPhone);
+        
+        lblEmail = new JLabel("이메일");
+        lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
+        lblEmail.setOpaque(true);
+        lblEmail.setBackground(SystemColor.activeCaption);
+        lblEmail.setForeground(Color.WHITE);
+        lblEmail.setFont(new Font("D2Coding", Font.BOLD, 24));
+        lblEmail.setBounds(12, 150, 120, 60);
+        frame.getContentPane().add(lblEmail);
+        
+        textEmail = new JTextField();
+        textEmail.setFont(new Font("D2Coding", Font.PLAIN, 24));
+        textEmail.setColumns(10);
+        textEmail.setBounds(144, 150, 328, 60);
+        frame.getContentPane().add(textEmail);
+        
+        btnInput = new JButton("입력");
+        btnInput.setFont(new Font("D2Coding", Font.BOLD, 24));
+        btnInput.setBounds(12, 220, 460, 60);
+        frame.getContentPane().add(btnInput);
+        
+        textArea = new JTextArea();
+        textArea.setFont(new Font("D2Coding", Font.PLAIN, 24));
+        textArea.setBounds(12, 290, 460, 161);
+        frame.getContentPane().add(textArea);
+    }
+}
